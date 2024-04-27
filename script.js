a650a88896c617e648300fd5a8f98d43adfc30d1
davisilva254 <davisilva.rogsantos@outlook.com>
2024-04-26 21:03:11 -0300

    Ajuste nas cores

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/AvatarDavi.png")
+    // Adiciona uma classe de ajuste
+    document.body.classList.add("light-mode-adjustment")
   } else {
     img.setAttribute("src", "./assets/avatar2.png")
+    // Remove a classe de ajuste se estiver presente
+    document.body.classList.remove("light-mode-adjustment")
   }
 }
